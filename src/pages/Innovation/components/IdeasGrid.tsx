1f63b08da9da2f6ff3125b24e8787c9deff4cb28
satish.attada <satish.attada@bp.com>
2026-01-06 15:31:58 +0000

    code chnages for error

--- src/pages/Innovation/components/IdeasGrid.tsx
+++ src/pages/Innovation/components/IdeasGrid.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import demoImage from '../../../public/assets/demos.png';
+import demoImage from '../../../../public/assets/demos.png';
 
 interface Idea {
   id: number;
